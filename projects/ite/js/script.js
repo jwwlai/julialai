45d71317dda3341c6457e185682dddf025571564
Julia Lai <jwwlai@gmail.com>
2013-12-08 19:33:46 -0800

    first commit
    adding all the things

---
+++ projects/ite/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * @author Julia Lai
+ */
+
+/*RESPONSIVE NAV*/
+$(document).ready(function() {
+	$(function() {
+		var pull = $('#pull');
+		menu = $('nav ul');
+		menuHeight = menu.height();
+
+		$(pull).on('click', function(e) {
+			e.preventDefault();
+			menu.slideToggle();
+		});
+		$(window).resize(function() {
+			var w = $(window).width();
+			if (w > 320 && menu.is(':hidden')) {
+				menu.removeAttr('style');
+			}
+		});
+	});
+}); 
